930de249801e30b798167dbcdf71a1f81f8df847
FengDenny <fengdennyy@gmail.com>
2021-07-14 23:30:14 -0700

    Added email/password form validation

--- client/src/pages/auth/Signin.js
+++ client/src/pages/auth/Signin.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SigninForm from "../../components/auth/SignInForm";
 import {
   MidHeader,
@@ -14,9 +14,50 @@ import { setUserLoggedIn } from "../../redux/actions/authAction";
 function Signin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
   const dispatch = useDispatch();
 
   const history = useHistory();
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+    if (email) {
+      emailFormValidation();
+    }
+    if (password) {
+      passwordFormValidation();
+    }
+  }, [email, password]);
+
+  const emailFormValidation = () => {
+    const emailRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email === "") {
+      setEmailError("Email address is required. ");
+    } else if (!emailRegex.test(email)) {
+      setEmailError("Enter a valid email address.");
+    } else {
+      setEmailError(null);
+    }
+  };
+
+  const passwordFormValidation = () => {
+    const passwordRegex = /^(?=.*\d).{8,}$/;
+
+    if (password === "") {
+      setPasswordError("Password is required.");
+    } else if (!passwordRegex.test(password)) {
+      setPasswordError("Password must be at least 8 characters.");
+    } else {
+      setPasswordError(null);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -50,6 +91,8 @@ function Signin() {
           password={password}
           setEmail={setEmail}
           setPassword={setPassword}
+          emailError={emailError}
+          passwordError={passwordError}
         />
       </ThemeProvider>
     </Container>
