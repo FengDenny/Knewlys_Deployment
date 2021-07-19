8b742a06cf921382b24159f390513bb9df5ee509
FengDenny <fengdennyy@gmail.com>
2021-07-18 17:16:45 -0700

    changed padding from 25px to 5px

--- client/src/styled-components/globalStyled.js
+++ client/src/styled-components/globalStyled.js
@@ -1,6 +1,6 @@
 import styled, { createGlobalStyle, keyframes } from "styled-components";
 import { motion } from "framer-motion";
-import { MediaQueries } from "./styled";
+import { MediaQueries, MediaHeight } from "./styled";
 
 // Global styling
 export const GlobalStyles = createGlobalStyle`
@@ -272,6 +272,9 @@ export const GridTwo = styled.div`
 
   ${MediaQueries("laptop")`
   grid-template-columns: repeat(1, 1fr);
+  `}
+  ${MediaHeight("mobileL")`
+   padding: 5px 0 0 0;
   `}
 `;
 
