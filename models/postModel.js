650c41e3842dccbdfc650b980b2999c959f1bafa
FengDenny <fengdennyy@gmail.com>
2021-07-20 16:34:06 -0700

    Post model schema

---
+++ models/postModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    minLength: 4,
+    maxLength: 150,
+  },
+  body: {
+    type: String,
+    required: [true, "Body is required"],
+    minLength: 4,
+    maxLength: 1000,
+  },
+  photo: { data: Buffer, contentType: String },
+  //   User Schema relationships
+  postedBy: { type: ObjectId, ref: "User" },
+  created: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const post = mongoose.model("Post", PostSchema);
+
+module.exports = post;
