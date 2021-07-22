815ae64fae137237db3b5b941b975d7452585d4d
FengDenny <fengdennyy@gmail.com>
2021-07-22 14:39:08 -0700

    added CUD handler factory function

--- controllers/handlersFactory.js
+++ controllers/handlersFactory.js
@@ -1,6 +1,15 @@
 const CatchAsync = require("../utility/CatchAsync");
 const AppError = require("../utility/AppError");
 
+// Create
+exports.createOne = (Model) =>
+  CatchAsync(async (req, res, next) => {
+    let doc = await Model.create(req.body);
+
+    res.status(200).json({ status: "success", data: { data: doc } });
+  });
+
+// Read
 exports.getAll = (Model, select) =>
   CatchAsync(async (req, res, next) => {
     let query = Model.find().select(select);
@@ -30,6 +39,43 @@ exports.getOne = (Model, select) =>
       results: doc.length,
       data: {
         data: doc,
+      },
+    });
+  });
+
+// Update (non-password)
+// MUST not be used for password update
+exports.updateOne = (Model) =>
+  CatchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.userID, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError(`No document found with that ID.`, 404));
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+// Delete
+exports.deleteOne = (Model) =>
+  CatchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.userID);
+
+    if (!doc) {
+      return next(new AppError(`No document found with that ID.`, 404));
+    }
+    res.status(200).json({
+      status: "success",
+      // no need to return any data
+      data: {
+        data: null,
       },
     });
   });
