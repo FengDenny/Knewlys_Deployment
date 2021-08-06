a87d92de36933e17c4ceb83f30cdcc5eed202a4d
FengDenny <fengdennyy@gmail.com>
2021-08-05 18:10:18 -0700

    axios request to get all post from backend to frontend

--- client/src/api/post/postAPI.js
+++ client/src/api/post/postAPI.js
@@ -4,3 +4,5 @@ export const createPost = async (token, userID, data) =>
   await axios.post(`/api/v1/post/${userID}`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
+
+export const getAllPosts = async () => await axios.get("/api/v1/post");
