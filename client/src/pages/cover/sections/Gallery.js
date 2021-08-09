51c055cd085d1e14325982d5b2a49ff28ea6572b
FengDenny <fengdennyy@gmail.com>
2021-08-09 00:45:04 -0700

    created SectionHeader styled-component/ added theme props

--- client/src/pages/cover/sections/Gallery.js
+++ client/src/pages/cover/sections/Gallery.js
@@ -12,6 +12,7 @@ import {
   GalleryBG,
   MediaFlexDisplay,
   GalleryFlexDisplay,
+  SectionHeader,
 } from "../../../styled-components/globalStyled";
 import { ThemeProvider } from "styled-components";
 import { useAnimation } from "framer-motion";
@@ -38,36 +39,41 @@ function Gallery() {
   return (
     <Section>
       <GalleryBG initial='hidden' animate={controls} ref={ref}>
-        <Container>
-          <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Container>
             <GallerySectionGrid>
-              <MediaFlexDisplay
-                theme={{ width50: "95%", height100: "50vh" }}
+              <MiddleFlexDisplayed
+                theme={{
+                  width50: "95%",
+                  flexDirection: "column",
+                }}
                 variants={leftVariants}
               >
-                <Header
-                  theme={{
-                    headerMain: "var(--white-color)",
-                    fontSizeXLG: "var(--font-size-48)",
-                    fontWeight: "bolder",
-                    width50: "90%",
-                  }}
-                >
-                  Your greatest moments in single gallery.
-                </Header>
-                <ParagraphWidth
-                  theme={{
-                    paragraphWidth: "434px",
-                    headerMain: "rgba(255, 255, 255, 0.83)",
-                    lineHeight: "26px",
-                    padding: "7px 0 0 0",
-                  }}
-                >
-                  Reminisce the past with your own gallery. Store your wedding
-                  memories to never forget the perfect past, including your
-                  vows.
-                </ParagraphWidth>
-              </MediaFlexDisplay>
+                <SectionHeader>
+                  <Header
+                    theme={{
+                      headerMain: "var(--white-color)",
+                      fontSizeXLG: "var(--font-size-48)",
+                      fontWeight: "bolder",
+                      width50: "90%",
+                    }}
+                  >
+                    Your greatest moments in single gallery.
+                  </Header>
+                  <ParagraphWidth
+                    theme={{
+                      paragraphWidth: "434px",
+                      headerMain: "rgba(255, 255, 255, 0.83)",
+                      lineHeight: "26px",
+                      padding: "7px 0 0 0",
+                    }}
+                  >
+                    Reminisce the past with your own gallery. Store your wedding
+                    memories to never forget the perfect past, including your
+                    vows.
+                  </ParagraphWidth>
+                </SectionHeader>
+              </MiddleFlexDisplayed>
               <GalleryFlexDisplay
                 theme={{ width50: "95%", height100: "50vh" }}
                 variants={rightVariants}
@@ -75,8 +81,8 @@ function Gallery() {
                 <GallerySectionImage src={gallery} alt='gallery' />
               </GalleryFlexDisplay>
             </GallerySectionGrid>
-          </ThemeProvider>
-        </Container>
+          </Container>{" "}
+        </ThemeProvider>
       </GalleryBG>
     </Section>
   );
