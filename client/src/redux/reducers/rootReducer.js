06b98539700d268738b7c2473e8ada32a075eb53
FengDenny <fengdennyy@gmail.com>
2021-08-10 14:53:59 -0700

    added footerReducer in rootReducer

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import resetReducer from "./resetReducer";
+import footerReducer from "./footerReducer";
 
 export default combineReducers({
   auth: authReducer,
   reset: resetReducer,
+  footer: footerReducer,
 });
