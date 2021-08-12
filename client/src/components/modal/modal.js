06989c022c363d7b708ba991c8ae48e9a74870c4
FengDenny <fengdennyy@gmail.com>
2021-08-12 13:17:33 -0700

    added modal context functions to modal.js for universal usage

--- client/src/components/modal/modal.js
+++ client/src/components/modal/modal.js
@@ -19,3 +19,36 @@ export const beforeClose = ({ setOpacity }) => {
     setTimeout(resolve, 200);
   });
 };
+
+// modal context
+
+const switchToSignup = (setActive) => {
+  setTimeout(() => {
+    setActive("signup");
+  }, 200);
+};
+
+const switchToSignin = (setActive) => {
+  setTimeout(() => {
+    setActive("signin");
+  }, 200);
+};
+
+const switchToForgotPassword = (setActive) => {
+  setTimeout(() => {
+    setActive("forgot");
+  }, 200);
+};
+
+const switchToRedirect = (setActive) => {
+  setTimeout(() => {
+    setActive("redirect");
+  }, 200);
+};
+
+export const contextValue = {
+  switchToSignup,
+  switchToSignin,
+  switchToForgotPassword,
+  switchToRedirect,
+};
