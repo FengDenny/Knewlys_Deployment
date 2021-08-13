942e12cb96fec85e8ea6f2e60c51a8a6cecc4267
FengDenny <fengdennyy@gmail.com>
2021-08-12 18:53:01 -0700

    Added postReducer to our rootReducer

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import resetReducer from "./resetReducer";
 import footerReducer from "./footerReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   auth: authReducer,
   reset: resetReducer,
   footer: footerReducer,
+  post: postReducer,
 });
