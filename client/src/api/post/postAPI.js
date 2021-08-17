3b037b5d0308a6ac13f8cda7d5259ea2af305851
FengDenny <fengdennyy@gmail.com>
2021-08-17 12:31:17 -0700

    get limited posts using req.query.limit

--- client/src/api/post/postAPI.js
+++ client/src/api/post/postAPI.js
@@ -4,8 +4,11 @@ export const createPost = async (token, userID, data) =>
   await axios.post(`/api/v1/post/${userID}`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
+// limit results
+// export const getAllPosts = async (limit = 21) =>
+//   await axios.get(`/api/v1/post?limit=${limit}`);
 
-export const getAllPosts = async () => await axios.get("/api/v1/post");
+export const getAllPosts = async () => await axios.get(`/api/v1/post`);
 
 export const getPost = async (postID) =>
   await axios.get(`/api/v1/post/${postID}`);
