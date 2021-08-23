bc08e265fece970b9978af0db78afd0754d4dd87
FengDenny <fengdennyy@gmail.com>
2021-08-22 20:11:54 -0700

    added route for user with params of userID for GET/PATCH/DELETE request/ added updateEmail for patch route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,7 @@ const {
 const {
   protect,
   updateUserPassword,
+  updateEmail,
 } = require("./../controllers/authController");
 
 // @desc Get all user
@@ -38,12 +39,11 @@ router.patch("/account/updatePassword", updateUserPassword);
 // @desc Get/Patch/Delete user for admins only
 //@route GET/Patch/Delete "/api/v1/user/:userID"
 // @access Private
-
-// router
-//   .route("/user/:userID")
-//   .get(getUser)
-//   .patch(updateUser)
-//   .delete(deleteUser);
+router
+  .route("/user/:userID")
+  .get(getAccount, getUser)
+  .patch(updateEmail)
+  .delete(deleteUser);
 
 // execute this first iff routes have :userID
 router.param("userID", userID);
